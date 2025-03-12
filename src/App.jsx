5140fb80a6cf13770614ad7b78bc167d843505a3
Bolaji87 <sakabolaji77@gmail.com>
2025-03-12 23:36:25 +0100

    feat: update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Appnav/Navbar";
 import Homepage from "./pages/Home/Homepage";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Homepage />} />
         </Routes>
       </BrowserRouter>
+      <Footer />
     </div>
   );
 }
