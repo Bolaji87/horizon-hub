83536e7f9f1c3256fd2c4eae2befe7a1618c7b6e
Bolaji87 <sakabolaji77@gmail.com>
2025-03-12 18:23:26 +0100

    feat: call NewsLetter in Homepage

--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Hero from "../../components/hero/Hero";
+import NewsLetter from "../../components/newsletter/NewsLetter";
+import styles from "./Homepage.module.css";
 
 function Homepage() {
   return (
-    <div>
+    <div className={styles.homePage}>
       <Hero />
+      <NewsLetter />
     </div>
   );
 }
