202de48ae165d505133f84c4d1ee319a9f72a749
Bolaji87 <sakabolaji77@gmail.com>
2025-03-12 02:39:18 +0100

    feat:update Navbar and styles

--- src/components/Appnav/Navbar.jsx
+++ src/components/Appnav/Navbar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import styles from "./Navbar.module.css";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className={styles.navbar}>
+      <h2>Horizon hub</h2>
+
+      <nav className={styles.listCont}>
+        <ul>
+          <li className={styles.listItem}>
+            <a href="">Home</a>
+          </li>
+          <li className={styles.listItem}>
+            <a href="">About</a>
+          </li>
+          <li className={styles.listItem}>
+            <a href="">Products</a>
+          </li>
+          <li className={styles.listItem}>
+            <a href="">cart</a>
+          </li>
+          <li className={`${styles.listItem} ${styles.login}`}>
+            <a href="">Login</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
