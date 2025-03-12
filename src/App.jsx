91dfe64d97732bbfa8bec5c09d24d77c81866639
Bolaji87 <sakabolaji77@gmail.com>
2025-03-12 04:05:51 +0100

    feat:use Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Appnav/Navbar";
+import Homepage from "./pages/Home/Homepage";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
