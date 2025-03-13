29e2402c1cd73034f9522f15ef4622b56b8fd3cd
Bolaji87 <sakabolaji77@gmail.com>
2025-03-13 18:13:31 +0100

    feat:add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy } from "react";
 
 import Navbar from "./components/Appnav/Navbar";
-import Homepage from "./pages/Home/Homepage";
 import Footer from "./components/footer/Footer";
+import Spinner from "./UI/Spinner";
+const Homepage = lazy(() => import("./pages/Home/Homepage"));
+const Products = lazy(() => import("./pages/products/Products"));
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-        </Routes>
-      </BrowserRouter>
-      <Footer />
+      <Suspense fallback={<Spinner />}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="products" element={<Products />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </Suspense>
     </div>
   );
 }
