cd620265b95a221c75bde92f36113044d28ff178
Bolaji87 <sakabolaji77@gmail.com>
2025-03-14 03:48:23 +0100

    feat: pass function to add items to cart

--- src/components/productcard/ProductCard.jsx
+++ src/components/productcard/ProductCard.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import styles from "./ProductCard.module.css";
+import { useCart } from "../../hooks/useCart";
 
 function ProductCard({ product }) {
   const { title, price, image, category, description, id } = product;
+  const { onAddToCart } = useCart();
+
   return (
     <li className={styles.card}>
       <p>{title}</p>
       <img className={styles.productImg} src={image} alt={title} />
       <p>{price}</p>
-      <button>Add to cart</button>
+      <button onClick={() => onAddToCart({ title, price, image, id })}>
+        Add to cart
+      </button>
     </li>
   );
 }
