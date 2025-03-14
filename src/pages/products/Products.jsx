6dda48754c20edb310e360fad7fc8f3950f86c23
Bolaji87 <sakabolaji77@gmail.com>
2025-03-14 03:48:36 +0100

    feat:update

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -2,24 +2,39 @@ import React, { useEffect, useState } from "react";
 import styles from "./Products.module.css";
 import Spinner from "../../UI/Spinner";
 import ProductCard from "../../components/productcard/ProductCard";
+import Error from "../../UI/Error";
 
 // const BASE_URL = "http://localhost:8000";
 const BASE_URL = "https://fakestoreapi.com/products";
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
     async function getProducts() {
-      const response = await fetch(`${BASE_URL}`);
-      const data = await response.json();
-      setProducts(data);
+      try {
+        setError("");
+        setIsLoading(true);
+        const response = await fetch(`${BASE_URL}`);
+        if (!response.ok)
+          throw new Error("There was an error fetching products");
+        const data = await response.json();
+        setProducts(data);
+        setIsLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setIsLoading(false);
+      }
     }
     getProducts();
   }, []);
   return (
     <div className={styles.products}>
-      {products.length > 0 ? (
+      {isLoading && <Spinner />}
+      {error && <Error errorMessage={error} />}
+      {!isLoading && !error && products.length > 0 && (
         <ul>
           {products.map(
             (product) =>
@@ -29,8 +44,6 @@ function Products() {
               )
           )}
         </ul>
-      ) : (
-        <Spinner />
       )}
     </div>
   );
