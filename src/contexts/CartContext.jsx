bb10d7639f8fd0058ddb360b851561a8813305c7
Bolaji87 <sakabolaji77@gmail.com>
2025-03-15 15:02:31 +0100

    feat: add case to handle cartitem deletion, create event handle function to handle cart item deletion

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -10,7 +10,7 @@ const initialState = {
 
 function reducer(state, action) {
   switch (action.type) {
-    case "add/cartItems": {
+    case "add/cart": {
       const newItem = action.payload;
       const existingItem = state.cartItems.find(
         (item) => item.id === newItem.id
@@ -39,6 +39,37 @@ function reducer(state, action) {
         ),
       };
     }
+    case "delete/cart": {
+      const newItemId = action.payload;
+      const existingItem = state.cartItems.find(
+        (item) => item.id === newItemId
+      );
+      let updatedCart;
+      if (existingItem) {
+        if (existingItem.quantity > 1) {
+          updatedCart = state.cartItems.map((item) =>
+            item.id === newItemId
+              ? { ...item, quantity: item.quantity - 1 }
+              : item
+          );
+        } else {
+          updatedCart = state.cartItems.filter((item) => item.id !== newItemId);
+        }
+      } else {
+        updatedCart = [...state.cartItems];
+      }
+
+      return {
+        ...state,
+        cartItems: updatedCart,
+        numOfItems: updatedCart.reduce((acc, item) => acc + item.quantity, 0),
+        totalAmount: updatedCart.reduce(
+          (acc, item) => acc + item.price * item.quantity,
+          0
+        ),
+      };
+    }
+
     default:
       throw new Error("Unknown action type");
   }
@@ -49,17 +80,25 @@ function CartProvider({ children }) {
     initialState
   );
 
-  function handleAddToCart(product) {
-    dispatch({ type: "add/cartItems", payload: product });
+  function handleAddToCartItems(product) {
+    dispatch({ type: "add/cart", payload: product });
   }
 
+  function handleDeleteCart(id) {
+    dispatch({ type: "delete/cart", payload: id });
+  }
+  // function handleClearCartItems() {
+  //   dispatch({ type: "clear/cartItems" });
+  // }
   return (
     <CartContext.Provider
       value={{
         cartItems,
         numOfItems,
         totalAmount,
-        onAddToCart: handleAddToCart,
+        onAddToCart: handleAddToCartItems,
+        onDeleteCartItem: handleDeleteCart,
+        // onClearCartItems: handleClearCartItems,
       }}
     >
       {children}
