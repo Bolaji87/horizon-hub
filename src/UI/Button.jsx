0b17f2ef7462b2402bc1ca0f45ba1c564b6670a0
Bolaji87 <sakabolaji77@gmail.com>
2025-03-15 15:16:51 +0100

    feat: pass in the onclick prop to navigate to products page

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Button({ children, className }) {
-  return <button className={className}>{children}</button>;
+function Button({ children, className, onclick }) {
+  return (
+    <button onClick={onclick} className={className}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
