743d242e10c28c0a340b01fef66f104d7790ad59
Bolaji87 <sakabolaji77@gmail.com>
2025-03-15 15:04:00 +0100

    feat: call event handler function to handle cart item deletion on the click of remove button

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 import styles from "./Cart.module.css";
+import { useCart } from "../../hooks/useCart";
+
 function Cart() {
-  return <div className={styles.cart}>Cart</div>;
+  const { cartItems, totalAmount, onDeleteCartItem } = useCart();
+
+  return (
+    <div className={styles.cart}>
+      {cartItems.length > 0 ? (
+        <>
+          <h2>
+            🛒 Your Cart (
+            {cartItems.reduce((acc, item) => acc + item.quantity, 0)} items)
+          </h2>
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.id} className={styles.cartItem}>
+                <span>
+                  {item.title} (x{item.quantity})
+                </span>
+                <img src={item.image} alt={item.title} />
+                <span>${item.price * item.quantity}</span>
+                <button onClick={() => onDeleteCartItem(item.id)}>
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+          <h2>Total: ${totalAmount.toFixed(2)}</h2>
+          <div>
+            <button className={styles.clearCart}>Clear Cart 🗑️</button>
+          </div>
+        </>
+      ) : (
+        <h1>Your cart is empty. Start adding items!</h1>
+      )}
+    </div>
+  );
 }
 
 export default Cart;
