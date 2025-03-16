526a1cbf975c24b64c9a94178e51c34224c1905c
Bolaji87 <sakabolaji77@gmail.com>
2025-03-16 14:43:22 +0100

    feat: fetch products category using useParams hooks, check if there is category then store  in allProducts state

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -3,48 +3,104 @@ import styles from "./Products.module.css";
 import Spinner from "../../UI/Spinner";
 import ProductCard from "../../components/productcard/ProductCard";
 import Error from "../../UI/Error";
+import { fetchProducts, fetchProductsCategory } from "../../utils";
+import { useParams } from "react-router-dom";
 
-// const BASE_URL = "http://localhost:8000";
-const BASE_URL = "https://fakestoreapi.com/products";
+const BASE_URL = `https://fakestoreapi.com`;
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [allProducts, setAllProducts] = useState([]);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const { category } = useParams();
 
-  useEffect(function () {
-    async function getProducts() {
-      try {
-        setError("");
-        setIsLoading(true);
-        const response = await fetch(`${BASE_URL}`);
-        if (!response.ok)
-          throw new Error("There was an error fetching products");
-        const data = await response.json();
-        setProducts(data);
-        setIsLoading(false);
-      } catch (err) {
-        setError(err.message);
-        setIsLoading(false);
+  useEffect(
+    function () {
+      if (!category) {
+        async function getProducts() {
+          try {
+            setIsLoading(true);
+            setError("");
+            const response = await fetchProducts();
+            if (!response)
+              throw new Error("There was an error fetching products");
+            setProducts(response);
+            setAllProducts(response);
+            setIsLoading(false);
+          } catch (err) {
+            setError(err.message);
+            setIsLoading(false);
+          }
+        }
+        getProducts();
+      } else {
+        async function getProductsCategory() {
+          try {
+            setIsLoading(true);
+            setError("");
+            const response = await fetchProductsCategory(category);
+            if (!response)
+              throw new Error("There was an error fetching products");
+            setProducts(response);
+            setIsLoading(false);
+          } catch (err) {
+            setError(err.message);
+            setIsLoading(false);
+          }
+        }
+        getProductsCategory();
       }
-    }
-    getProducts();
-  }, []);
+    },
+    [category]
+  );
+
+  function handleFilterProducts(productCategory = null) {
+    const filteredProducts = allProducts.filter(
+      (product) => product.category === productCategory
+    );
+
+    setProducts(productCategory ? filteredProducts : allProducts);
+  }
+
+  const ALLOWED_CATEGORIES = {
+    MEN: "men's clothing",
+    WOMEN: "women's clothing",
+  };
   return (
-    <div className={styles.products}>
-      {isLoading && <Spinner />}
-      {error && <Error errorMessage={error} />}
-      {!isLoading && !error && products.length > 0 && (
-        <ul>
-          {products.map(
-            (product) =>
-              product.category !== "jewelery" &&
-              product.category !== "electronics" && (
-                <ProductCard key={product.id} product={product} />
-              )
-          )}
-        </ul>
-      )}
+    <div className={styles.productsCont}>
+      <div className={styles.category}>
+        <div className={styles.all} onClick={() => handleFilterProducts()}>
+          All
+        </div>
+        <div
+          className={styles.women}
+          onClick={() => handleFilterProducts(ALLOWED_CATEGORIES.WOMEN)}
+        >
+          Abayas
+        </div>
+        <div
+          className={styles.men}
+          onClick={() => handleFilterProducts(ALLOWED_CATEGORIES.MEN)}
+        >
+          Jalabs
+        </div>
+      </div>
+      <div className={styles.products}>
+        {isLoading && <Spinner />}
+        {error && <Error errorMessage={error} />}
+        {!isLoading && !error && products.length > 0 && (
+          <ul>
+            {products.map(
+              (product) =>
+                product.category !== "jewelery" &&
+                product.category !== "electronics" && (
+                  <ProductCard key={product.id} product={product} />
+                )
+            )}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
