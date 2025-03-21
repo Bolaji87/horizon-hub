7a5d344f5f938c9dc4281aa94661fa9011f62be1
Bolaji87 <sakabolaji77@gmail.com>
2025-03-21 17:03:54 +0100

    feat: call ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,46 @@ import Navbar from "./components/Appnav/Navbar";
 import Footer from "./components/footer/Footer";
 import Spinner from "./UI/Spinner";
 import { CartProvider } from "./contexts/CartContext";
+import { AuthProvider } from "./contexts/AuthContext";
 
 const Homepage = lazy(() => import("./pages/Home/Homepage"));
 const Products = lazy(() => import("./pages/products/Products"));
 const Cart = lazy(() => import("./pages/cart/Cart"));
 const AboutUs = lazy(() => import("./pages/about/AboutUs"));
+const Login = lazy(() => import("./pages/login/Login"));
+const Checkout = lazy(() => import("./pages/checkout/Checkout"));
+const ProtectedRoute = lazy(() => import("./pages/ProtectedRoute"));
 const PageNotFound = lazy(() => import("./pages/notfound/PageNotFound"));
 
 function App() {
   return (
-    <CartProvider>
-      <Suspense fallback={<Spinner />}>
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="products" element={<Products />} />
-            <Route path="cart" element={<Cart />} />
-            <Route path="about" element={<AboutUs />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </Suspense>
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        <Suspense fallback={<Spinner />}>
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="products/:category?" element={<Products />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="about" element={<AboutUs />} />
+              <Route path="login" element={<Login />} />
+
+              <Route
+                path="checkout"
+                element={
+                  <ProtectedRoute>
+                    <Checkout />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+            <Footer />
+          </BrowserRouter>
+        </Suspense>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
