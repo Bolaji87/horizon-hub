e88f4ba0ec9f274b5cbd8507e07e88d98b81e5c3
Bolaji87 <sakabolaji77@gmail.com>
2025-03-21 17:01:59 +0100

    feat: update cart page and styles

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -15,54 +15,63 @@ function Cart() {
         🛒 Your Cart ({cartItems.reduce((acc, item) => acc + item.quantity, 0)}{" "}
         items)
       </h2>
-      <div className={styles.cart}>
-        {cartItems.length > 0 ? (
-          <>
-            <div className={styles.cartWrapper}>
-              <div className={styles.section}>
-                <ul>
-                  {cartItems.map((item) => (
-                    <>
-                      <div className={styles.imageCont}>
-                        <img
-                          src={item.image}
-                          alt={item.title}
-                          className={styles.image}
-                        />
-                      </div>
-                      <li key={item.id} className={styles.cartItem}>
-                        <span>
-                          {item.title} (x{item.quantity})
-                        </span>
-                        <span>${item.price * item.quantity}</span>
-                        <button onClick={() => onDeleteCartItem(item.id)}>
-                          Remove
-                        </button>
-                      </li>
-                    </>
-                  ))}
-                </ul>
-                <div>
-                  <button
-                    className={styles.clearCart}
-                    onClick={onClearCartItems}
-                  >
-                    Clear Cart 🗑️
-                  </button>
-                </div>
+      <>
+        <div className={styles.cart}>
+          {cartItems.length > 0 ? (
+            <>
+              <ul>
+                {cartItems.map((item) => (
+                  <>
+                    <img
+                      src={item.image}
+                      alt={item.title}
+                      className={styles.image}
+                    />
+
+                    <li key={item.id} className={styles.cartItem}>
+                      <span>
+                        {item.title} (x{item.quantity})
+                      </span>
+                      <button className={styles["free-shipping"]}>
+                        Free shipping
+                      </button>
+                      <h2 className={styles["cart-item-h2"]}>{item.title}</h2>
+                      <h3 className="cart-item-h3">
+                        ${(item.price * 1.2).toFixed(2)}
+                      </h3>
+                      <h1 className={styles["cart-item-h1"]}>
+                        ${item.price.toFixed(2)}
+                      </h1>
+                      <h4 className={styles["cart-item-h4"]}>
+                        {item.description.length > 200
+                          ? `${item.description.substring(0, 200)}...`
+                          : item.description}
+                      </h4>
+                      <button onClick={() => onDeleteCartItem(item.id)}>
+                        Remove
+                      </button>
+                    </li>
+                  </>
+                ))}
+              </ul>
+
+              <div className={styles.cartSummary}>
+                <button className={styles.clearCart} onClick={onClearCartItems}>
+                  Clear Cart 🗑️
+                </button>
+                <h2>Total: ${totalAmount.toFixed(2)}</h2>
+                <span>shipping cost : 0$</span>
+                <span>total items : x</span>
+                <button onClick={() => navigate("/checkout")}>Checkout</button>
               </div>
-            </div>
-            <div className={styles.cartSummary}>
-              <h2>Total: ${totalAmount.toFixed(2)}</h2>
-              <span>shipping cost : 0$</span>
-              <span>total items : x</span>
-              <button onClick={() => navigate("/checkout")}>Checkout</button>
-            </div>
-          </>
-        ) : (
-          <h1>Your cart is empty. Start adding items!</h1>
-        )}
-      </div>
+            </>
+          ) : (
+            <h1 className={styles.emptyText}>
+              Your cart is empty. Start adding items!
+            </h1>
+          )}
+        </div>
+      </>
     </div>
   );
 }
