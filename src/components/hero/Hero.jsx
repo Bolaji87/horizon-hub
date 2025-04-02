51e7b5d985f1fbfbbe24c2f94b80fb5a6a82d1a0
Bolaji87 <sakabolaji77@gmail.com>
2025-04-02 03:57:11 +0100

    feat: change styles to tailwind

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -29,7 +29,7 @@ const imageData = [
 
 function Hero() {
   return (
-    <article className={styles.heroContainer}>
+    <article className="flex min-h-screen w-full justify-between px-8 py-3">
       <WelcomeNotes />
       <WelcomeMedia imageData={imageData} />
     </article>
@@ -39,9 +39,12 @@ function Hero() {
 function WelcomeNotes() {
   const navigate = useNavigate();
   return (
-    <section className={styles.notes}>
-      <h1>Welcome to Horizon Hub</h1>
-      <p>
+    <section className="flex flex-col justify-center">
+      <h1 className="text-2xl font-bold text-amber-500">
+        Welcome to Horizon Hub
+      </h1>
+
+      <p className="mb-6 mt-5 text-xl font-semibold italic leading-7 text-amber-700">
         Experience the perfect blend of <strong>beauty</strong>,{" "}
         <strong>elegance,</strong>
         <br /> and <strong>comfort</strong> with our exquisite Abayas and
@@ -53,15 +56,21 @@ function WelcomeNotes() {
     </section>
   );
 }
+
 function WelcomeMedia({ imageData }) {
   return (
-    <ul className={styles.media}>
+    <ul className="grid h-[250px] grid-cols-2 gap-3 gap-y-7 pt-9">
       {imageData.map((data) => (
         <li key={data.id}>
-          <img className={styles.heroImage} src={data.image} alt={data.name} />
+          <img
+            className="h-[180px] w-[250px] rounded-md bg-white object-cover"
+            src={data.image}
+            alt={data.name}
+          />
         </li>
       ))}
     </ul>
   );
 }
+
 export default Hero;
