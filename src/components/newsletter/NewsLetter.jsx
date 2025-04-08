33c145fcba5f3e2bb61308b3e9788ae2341f73d3
Bolaji87 <sakabolaji77@gmail.com>
2025-04-08 13:04:05 +0100

    feat:convert styling to tailwind

--- src/components/newsletter/NewsLetter.jsx
+++ src/components/newsletter/NewsLetter.jsx
@@ -1,22 +1,24 @@
 import React from "react";
-import styles from "./NewsLetter.module.css";
 import Button from "../../UI/Button";
 
 function NewsLetter() {
   return (
-    <section className={styles.newsLetter}>
-      <h1>
+    <section className="flex min-h-fit flex-col items-center justify-center bg-pink-300 py-10 text-purple-900">
+      <h1 className="mb-4 text-3xl font-semibold">
         Get our stories delivered From <br />
-        <span className={styles.bigText}> us to your inbox weekly.</span>
+        <span className="flex items-center justify-center">
+          {" "}
+          us to your inbox weekly.
+        </span>
       </h1>
-      <form className={styles.newsForm} action="">
-        <input type="email" placeholder="Your Email" />
-        <Button className={styles.formBtn}>get started</Button>
+      <form action="" className="mb-4 flex gap-2 sm:gap-5">
+        <input className="w-72 md:w-96" type="email" placeholder="Your Email" />
+        <Button type="cto">get started</Button>
       </form>
-      <p>
+      <p className="text-xl leading-7">
         Get a response tomorrow if you submit by 9pm today. If we received after{" "}
         <br />
-        <span className={styles.smallText}>
+        <span className="flex items-center justify-center">
           9pm will get a reponse the following day.
         </span>
       </p>
